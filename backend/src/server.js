43cd448f505153c2924fcbc05c73a6ae8a8c231c
nayyelin-2002 <yelinnay4@gmail.com>
2025-08-14 16:19:40 +0700

    added

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://prepwise-ai.netlify.app",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
