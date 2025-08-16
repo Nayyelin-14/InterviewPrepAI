74f998f73e8b544d4cf7025a27af552d9a1d1c17
nayyelin-2002 <yelinnay4@gmail.com>
2025-08-16 16:35:26 +0700

    final update

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://interview-prep-ai-nine.vercel.app",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
